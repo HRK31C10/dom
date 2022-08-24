0c5be2aa1aa568605890e7984a84bd86f652dfda
HERICK CEZAR DIAS ANDRADE <herick.cdandrade@senacsp.edu.br>
2022-08-24 11:33:48 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+function clicou(){ 
+    document.querySelector('#area').innerHTML = "Clicou Aqui"
+}
+
+function entrou(){ 
+    document.querySelector('#area').innerHTML = "Passou o mouse"
+}
+
+function fora(){ 
+    document.querySelector('#area').innerHTML = "Tirou o mouse"
+}
---
+++ soma.js
@@ -0,0 +1,5 @@
+function soma(){
+   let n1 = document.querySelector('#n1').value
+   let n2 = document.querySelector('#n2').value 
+   document.querySelector('#resultado').innerHTML = parseInt(n1)+parseInt(n2)
+}
